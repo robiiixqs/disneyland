d9381b6a6aa68807a438a46b20a619d1dbc72eed
apple <robertagavriloaie98@gmail.com>
2026-06-22 01:18:29 +0100

    Add CSV dataset loading

--- process.py
+++ process.py
@@ -2,4 +2,4 @@ import csv
 def load_data(file_path):
     "**read the csv file and return low in a list"""
     with open(file_path, "r", encoding="utf-8-sig", newline="") as csv_file:
-        return list(csv.DictReader(csv_file))
+           return list(csv.DictReader(csv_file))
